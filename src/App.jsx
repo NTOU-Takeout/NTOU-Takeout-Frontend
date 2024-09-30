35bdd52012a6391ee183d3e49920de7419de4d67
ZIhan <enboy5219@gmail.com>
2024-09-30 23:23:03 +0800

    init via files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App(){
+    return (
+        <div>
+            <h1>My First React App</h1>
+            <p>Welcome to my first React app</p>
+        </div>
+    )
+}
+
+export default App
