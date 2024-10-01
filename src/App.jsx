7757bc9efe3a00bbf05f48292d6bb1d9f875d2f6
ZIhan <enboy5219@gmail.com>
2024-10-01 13:11:31 +0800

    add via files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+//import {owo} from './assets/card.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookmark as hollowFaBookmark } from '@fortawesome/free-regular-svg-icons/faBookmark';//hollow
+import { faBookmark as solidFaBookmark } from '@fortawesome/free-solid-svg-icons/faBookmark';
+
 function App(){
     return (
         <div>
-            <h1>My First React App</h1>
+            
+            <FontAwesomeIcon icon={hollowFaBookmark} />
+            <FontAwesomeIcon icon={solidFaBookmark} />
+            <h1>My First React App asfasfas</h1>
             <p>Welcome to my first React app</p>
         </div>
     )
--- src/components/merchant.jsx
+++ src/components/merchant.jsx
@@ -1,7 +1,10 @@
 import React from "react"
-import bookmark from '../assets/bookmark.svg'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookmark as hollowFaBookmark } from '@fortawesome/free-regular-svg-icons/faBookmark';//hollow
+import { faBookmark as solidFaBookmark } from '@fortawesome/free-solid-svg-icons/faBookmark';
+import { faStar as hollowStar } from "@fortawesome/free-regular-svg-icons";
+import { faStar as solidStar } from "@fortawesome/free-solid-svg-icons";
 const merchant = ( {name, distance,costDownLimit,costUpLimit,starRate,starNumber } ) => {
-    
+
 
 }
