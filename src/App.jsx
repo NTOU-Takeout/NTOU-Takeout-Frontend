3c3616782dbf49a330f82d0d3c729be41428bc69
YuYutw123 <yuyutw123@gmail.com>
2024-10-02 10:42:01 +0000

    Remove merchangtPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import Header from './components/Header';
-import MenuHeader from './components/merchantPage/MenuHeader';
 
 function App() {
     return (
         <div>
             <Header></Header>
-            {/* <MenuHeader></MenuHeader> */}
         </div>
     )
 }
