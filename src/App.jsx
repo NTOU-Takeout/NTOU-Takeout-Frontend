ee927913c7e0b641edac581cb7178062691ee450
YuYutw123 <yuyutw123@gmail.com>
2024-10-06 19:21:57 +0000

    Fix conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import Header from './components/Header';
+import Merchant from './components/Merchant';
+import Searchbar from './components/Searchbar';
+import MenuHeader from './components/merchantPage/MenuHeader';
 
 function App() {
+    //{name, distance, costDownLimit, costUpLimit, starRate, starNumber }
     return (
         <div>
-            {/* <Header></Header> */}
-            <MenuHeader></MenuHeader>
+            <Header></Header>
+            <Searchbar></Searchbar>
+            <Merchant 
+                id={0}
+                name="海洋大學店" 
+                distance={1.6} 
+                costDownLimit={98} 
+                costUpLimit={123} 
+                starRate={4.8} 
+                starNumber={71} 
+                >
+            </Merchant>
+            <Merchant 
+                id={1}
+                name="海" 
+                distance={2.5} 
+                costDownLimit={12} 
+                costUpLimit={124} 
+                starRate={3.2} 
+                starNumber={12} >
+            </Merchant>
+            {/* <MenuHeader></MenuHeader> */}
         </div>
     )
 }
-
 export default App;
