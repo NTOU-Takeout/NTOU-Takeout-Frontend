32a8a967e4847d9fb786917bef37c1c8e846b4a8
YuYutw123 <yuyutw123@gmail.com>
2024-10-06 19:03:36 +0000

    Update: blur banner, add information div.

--- src/components/merchantPage/MenuHeader.jsx
+++ src/components/merchantPage/MenuHeader.jsx
@@ -5,13 +5,13 @@ import { faArrowLeft, faShareNodes, faStar, faInfoCircle } from '@fortawesome/fr
 import useLoadingStore from '../../stores/loadingStore';
 
 const MenuHeader = ({
-    title = '海洋大學店',          // 預設標題
-    distance = 100,               // 預設距離
-    averageCost = 800,           // 預設平均消費
-    rating = 4.2,                // 預設評分
-    reviews = 100,               // 預設評價數
-    informationLink = 'https://google.com', // 預設資訊連結
-    bannerLink = 'https://i.imgur.com/S1OPVB6.jpeg', // 預設橫幅連結
+    title = '海洋大學店',          // default title
+    distance = 100,               // default distance
+    averageCost = 800,           // default average cost
+    rating = 4.2,                // default rating
+    reviews = 100,               // default reviews
+    informationLink = 'https://google.com', // defaults information link
+    bannerLink = 'https://i.imgur.com/S1OPVB6.jpeg', // default banner link
 }) => {
     
     // get state from store
@@ -35,9 +35,9 @@ const MenuHeader = ({
     }
     
     return (
-        <header className="menu-header">
+        <header className="fixed top-0 left-0 w-full width-100 menu-header">
             <div
-                className="banner bg-cover bg-center h-64 relative filter blur-sm"
+                className="banner bg-cover bg-center h-64 relative before:content-[''] before:absolute before:w-full before:h-full before:backdrop-blur-sm"
                 style={{ backgroundImage: `url(${bannerLink})` }}
             >
             </div>
@@ -47,18 +47,18 @@ const MenuHeader = ({
             <div className="pt-1 pb-1 pl-2 pr-2 share-btn absolute top-10 right-4 transform -translate-y-1/2 bg-white/60 rounded-full">
                 <FontAwesomeIcon icon={faShareNodes} className="text-slate-800" />
             </div>
-            {/* 商家資訊部分 */}
+            {/* Store information */}
             <div className="bg-white rounded-t-2xl p-4 relative -top-24 left-0 right-0 z-10">
                 <div className="flex items-center justify-between">
                     <div className="flex flex-col">
-                        <h2 className="text-xl font-bold">商家名稱</h2>
-                        <p className="text-gray-400 text-sm">距離您約...公里</p>
-                        <p className="text-green-600 text-sm mt-1">平均花費約100~200</p>
+                        <h2 className="text-xl font-bold">{title}</h2>
+                        <p className="text-gray-400 text-sm">距離您約{distance}公里</p>
+                        <p className="text-green-600 text-sm mt-1">平均花費約{averageCost}元</p>
                     </div>
-                    <div className="flex items-center">
-                        <span className="text-xl font-semibold">4.8</span>
-                        <span className="text-gray-400 ml-1">(100+)</span>
-                        <FontAwesomeIcon icon={faStar} className="text-yellow-400 ml-2" />
+                    <div className="flex items-center mt-10">
+                        <FontAwesomeIcon icon={faStar} className="text-yellow-400 ml-2 mt-1" />
+                        <span className="text-gray-400 ml-1 mt-0.5">({reviews})</span>
+                        <span className="text-xl font-semibold">&nbsp;{rating}</span>
                     </div>
                     <div className="absolute top-4 right-4 text-xl text-gray-500">
                         <FontAwesomeIcon icon={faInfoCircle} />
