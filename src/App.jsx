e08f868fefaf2bd5c07839ce6b27b3a2970fdcc0
YuYutw123 <yuyutw123@gmail.com>
2024-10-08 18:27:20 +0000

    Uncomment searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import Header from './components/Header';
 import Merchant from './components/Merchant';
+import useSidebarStore from './stores/sidebarStore';
+import Sidebar from './components/Sidebar';
 import Searchbar from './components/Searchbar';
 import MenuHeader from './components/merchantPage/MenuHeader';
 
 function App() {
-    //{name, distance, costDownLimit, costUpLimit, starRate, starNumber }
+    const toggleSidebar= useSidebarStore((state) => state.toggleSidebar);
+    const isOpen=useSidebarStore((state)=>state.isOpen);
+    const closeSidebar=useSidebarStore((state)=>state.closeSidebar);
+
     return (
+        <div 
+        >
+            <Header
+                onLeftClick={toggleSidebar}
+            />
+            <Sidebar></Sidebar>
+            
+            <Searchbar></Searchbar>
+            {/* <Merchant 
         <div>
             <Header></Header>
-            <Searchbar></Searchbar>
             <Merchant 
                 id={0}
                 name="海洋大學店" 
