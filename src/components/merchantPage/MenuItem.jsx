fcc39962ba2cedbc7e5a0a516cd39806af00399d
YuYutw123 <yuyutw123@gmail.com>
2024-10-08 18:09:11 +0000

    Add menu item

--- src/components/merchantPage/MenuItem.jsx
+++ src/components/merchantPage/MenuItem.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const MenuItemCard = () => {
+const MenuItemCard = ({
+    itemName = '原味飯糰',          // default itemName
+    price = 30,               // default price
+    description = '綠聽吧之位子宜寧，各話又，正見跳免還「沒古六更是三肖」。',
+}) => {
   return (
     <div className="MenuItem block">
       <div className="flex m-8 max-w-xl bg-white text-white rounded-lg overflow-hidden shadow-lg">
-        {/* 圖片部分 */}
+        {/* Image */}
         <div className="w-1/2">
           <img 
             src="https://picsum.photos/200/150" 
@@ -13,20 +17,20 @@ const MenuItemCard = () => {
           />
         </div>
 
-        {/* 內容部分 */}
+        {/* Content */}
         <div className="w-2/3 p-4">
-          {/* 標題 */}
-          <h2 className="text-2xl font-bold mb-2 text-black">原味飯糰</h2>
+          {/* Title */}
+          <h2 className="text-2xl font-bold mb-2 text-black">{itemName}</h2>
 
-          {/* 價格 */}
-          <p className="text-xl text-gray-800">$30</p>
+          {/* Price */}
+          <p className="text-xl text-gray-800">${price}</p>
 
-          {/* 描述 */}
+          {/* Description */}
           <p className="text-sm text-gray-600 mt-2">
-            綠聽吧之位子宜寧，各話又，正見跳免還「沒古六更是三肖」。
+            {description}
           </p>
 
-          {/* 加號按鈕 */}
+          {/* Add button */}
           <div className="flex justify-end mt-4">
             <button className="bg-orange-500 text-white rounded-full w-10 h-10 flex items-center justify-center">
               <span className="text-2xl font-bold mb-1">+</span>
