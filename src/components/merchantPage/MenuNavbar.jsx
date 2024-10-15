ab596bb7cb6dc896601ee09991d296b7686c8220
YuYutw123 <yuyutw123@gmail.com>
2024-10-15 19:38:05 +0000

    Add Menu Navbar.

---
+++ src/components/merchantPage/MenuNavbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import useNavStore from '../../stores/merchantMenuNav';
+
+const Navbar = () => {
+    const { navbarItems, nowPage, setNowPage } = useNavStore();
+
+    return (
+    <nav className="bg-white p-4">
+      {/* Navbar Items */}
+      <ul className="mt-2 flex space-x-4 text-gray-400 text-lg font-notoTC">
+        {navbarItems.map((item, index) => (
+          <li
+            key={index}
+            className={`${
+              index === nowPage ? 'text-black font-bold border-b-2 border-black hover:border-black' : ''
+            } hover:text-gray-800 cursor-pointer`}
+            onClick={() => setNowPage(index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
