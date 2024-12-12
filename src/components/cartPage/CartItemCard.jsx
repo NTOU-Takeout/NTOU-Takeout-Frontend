3ce9f8f09ec087b9f6ac5c32a615181f9a11c1f1
Yuankai619 <Yuankai_@outlook.com>
2024-12-13 03:51:54 +0800

    update delete icon and function

--- src/components/cartPage/CartItemCard.jsx
+++ src/components/cartPage/CartItemCard.jsx
@@ -1,6 +1,8 @@
 import PropTypes from "prop-types";
 import { useCartUpdateMutation } from "../../hooks/cart/useCartUpdateMutation";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 const CartItemCard = ({ dishData, imageUrl }) => {
     const {
         id,
@@ -12,11 +14,12 @@ const CartItemCard = ({ dishData, imageUrl }) => {
         note
     } = dishData;
     const [nowQuantity, setNowQuantity] = useState(quantity);
-    const { patchCartAsync, patchCartPending } = useCartUpdateMutation();
+    const { patchCartAsync } = useCartUpdateMutation();
 
     const handleQuantityChange = (change) => {
         const newQ = nowQuantity + change;
-        if (newQ <= 0) return;
+        if (newQ < 0 || newQ > 25) return;
+
 
         // optimistic update
         setNowQuantity(newQ);
@@ -33,38 +36,43 @@ const CartItemCard = ({ dishData, imageUrl }) => {
         : "";
 
     return (
-        <div className="relative flex rounded-lg p-4 w-full items-start ">
-            <img
-                src={imageUrl}
-                alt={dishName}
-                className="w-20 h-26 object-cover rounded-lg flex-shrink-0"
-            />
-            <div className="ml-4 flex min-w-0 flex-col h-full" >
-                <h2 className="text-lg font-semibold truncate">{dishName}</h2>
-                {formattedAttributes && <p className="text-sm text-gray-500 truncate">
-                    {formattedAttributes}(+${totalExtraCost})
-                </p>}
-                {note && <p className="text-sm text-gray-500 line-clamp-2">{note}</p>}
-                <p className="text-xl mt-2 absolute bottom-[15px]">
-                    $ {price}
-                </p>
+        nowQuantity <= 0 ? null :
+            <div className="relative flex rounded-lg p-4 w-full items-start ">
+                <img
+                    src={imageUrl}
+                    alt={dishName}
+                    className="w-20 h-26 object-cover rounded-lg flex-shrink-0"
+                />
+                <div className="ml-4 flex min-w-0 flex-col h-full" >
+                    <h2 className="text-lg font-semibold truncate">{dishName}</h2>
+                    {formattedAttributes && <p className="text-sm text-gray-500 truncate">
+                        {formattedAttributes}(+${totalExtraCost})
+                    </p>}
+                    {note && <p className="text-sm text-gray-500 line-clamp-2">{note}</p>}
+                    <p className="text-xl mt-2 absolute bottom-[15px]">
+                        $ {price}
+                    </p>
+                </div>
+                <div className="absolute bottom-[15px] right-[15px] flex items-end border border-gray-300 rounded-md">
+                    <button
+                        onClick={() => handleQuantityChange(-1)}
+                        className="px-2 py-0 text-lg rounded-l-md w-7"
+                    >
+                        {
+                            nowQuantity <= 1 ?
+                                <FontAwesomeIcon icon={faTrashCan} size="xs" style={{ color: "#d00b0b", }} /> :
+                                "-"
+                        }
+                    </button>
+                    <span className="px-4 py-0.5">{nowQuantity}</span>
+                    <button
+                        onClick={() => handleQuantityChange(1)}
+                        className="px-2 py-0 text-lg rounded-r-md w-7"
+                    >
+                        +
+                    </button>
+                </div>
             </div>
-            <div className="absolute bottom-[15px] right-[15px] flex items-end border border-gray-300 rounded-md">
-                <button
-                    onClick={() => handleQuantityChange(-1)}
-                    className="px-2 py-0 text-lg rounded-l-md"
-                >
-                    -
-                </button>
-                <span className="px-4 py-0.5">{nowQuantity}</span>
-                <button
-                    onClick={() => handleQuantityChange(1)}
-                    className="px-2 py-0 text-lg rounded-r-md"
-                >
-                    +
-                </button>
-            </div>
-        </div>
     );
 };
 
