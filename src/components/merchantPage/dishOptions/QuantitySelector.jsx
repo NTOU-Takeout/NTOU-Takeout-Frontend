b85ba5e91a0038661e82958329c87d017c3e17f2
Yuankai619 <Yuankai_@outlook.com>
2024-12-13 04:29:02 +0800

    set quantity limit

--- src/components/merchantPage/dishOptions/QuantitySelector.jsx
+++ src/components/merchantPage/dishOptions/QuantitySelector.jsx
@@ -1,23 +1,24 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 import useDishStore from "../../../stores/dishDetailStore.js";
 
 const QuantitySelector = ({ dishId }) => {
-    const { dishes, setQuantity } = useDishStore();
-    const quantity = dishes[dishId]?.quantity || 0;
+    const [quantity, setQuantity] = useState(1);
 
     return (
-        <div className="font-notoTC flex items-center bg-orange-500 rounded-full px-2 py-1 shadow-md border border-orange-700 mr-4">
+        <div className="font-notoTC flex flex-row items-center justify-center bg-orange-500 rounded-full px-2 py-1 shadow-md border border-orange-700 mr-4 ">
             <button
                 className="pb-1 bg-orange-500 text-white font-bold rounded-full w-6 h-6 flex items-center justify-center text-3xl mb-1"
-                onClick={() => setQuantity(dishId, quantity - 1)}
-                disabled={quantity <= 0}
+                onClick={() => setQuantity(prev => prev - 1)}
+                disabled={quantity <= 1}
             >
                 -
             </button>
-            <span className="pb-1 mx-4 text-white font-bold">{quantity}</span>
+            <span className="pb-1 w-8 mx-2 text-center text-white font-bold">{quantity}</span>
             <button
                 className="bg-orange-500 text-white font-bold rounded-full w-6 h-6 flex items-center justify-center text-xl"
-                onClick={() => setQuantity(dishId, quantity + 1)}
+                onClick={() => setQuantity(prev => prev + 1)}
+                disabled={quantity >= 25}
             >
                 +
             </button>
