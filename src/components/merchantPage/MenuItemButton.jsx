f69c572ea0f440b27401875cf037691cfc0a95df
Yuankai619 <Yuankai_@outlook.com>
2024-12-14 21:49:09 +0800

    add storeId condition

--- src/components/merchantPage/MenuItemButton.jsx
+++ src/components/merchantPage/MenuItemButton.jsx
@@ -1,12 +1,16 @@
 import PropTypes from 'prop-types';
+import { useParams } from 'react-router-dom';
 import { useSystemContext } from '../../context/SystemContext';
 const MenuItemButton = ({ dishId, onClick }) => {
+    const { merchantId } = useParams();
     const { cartData } = useSystemContext();
     let displayText = '+';
-    const totalQuantity = cartData.orderedDishes
-        .filter(d => d.dishId == dishId)
-        .reduce((sum, d) => sum + d.quantity, 0);
-    if (totalQuantity > 0) displayText = totalQuantity;
+    if (cartData?.storeId === merchantId) {
+        const totalQuantity = cartData.orderedDishes
+            .filter(d => d.dishId == dishId)
+            .reduce((sum, d) => sum + d.quantity, 0);
+        if (totalQuantity > 0) displayText = totalQuantity;
+    }
     const isPlus = displayText === '+';
     const btnClasses = isPlus
         ? 'bg-orange-500 text-white'
