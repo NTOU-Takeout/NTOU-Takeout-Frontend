8e7a974a99cb88a5ee7c69dc8c9e95168050ddd2
Yuankai619 <Yuankai_@outlook.com>
2024-12-15 23:04:24 +0800

    Refactor Header
    can pass right components into Header

--- src/components/storePage/home/Header.jsx
+++ src/components/storePage/home/Header.jsx
@@ -5,28 +5,43 @@ import { faList } from "@fortawesome/free-solid-svg-icons";
 const Header = ({
     title,
     leftIcon = faList,
-    onLeftClick = () => { },
+    onLeftClick,
+    rightComponents = [],
 }) => {
+    console.log('Header rightComponents:', rightComponents);
+    console.log('Header rightComponents length:', rightComponents.length);
+    console.log('Header rightComponents type:', typeof rightComponents);
     return (
-        <header className="fixed z-30 top-0 left-0 w-full flex justify-center items-center bg-white  transition-shadow duration-300 ease-in-out p-2 font-notoTC shadow-md
-        ">
+        <header
+            className="fixed z-30 top-0 left-0 w-full flex flex-row items-center justify-between bg-white  transition-shadow duration-300 ease-in-out p-3 font-notoTC shadow-md"
+        >
             <div
                 className="absolute left-4 text-xl cursor-pointer"
                 onClick={onLeftClick}
             >
                 <FontAwesomeIcon icon={leftIcon} />
             </div>
-            <h1 className="font-noto font-bold text-2xl m-0 text-center">
+            <h1 className="font-bold text-xl ml-12">
                 <a href="/merchantPage">{title}</a>
             </h1>
+            {rightComponents.length > 0 && (
+                <div className="flex items-center gap-4">
+                    {rightComponents.map((Component, index) => (
+                        <div key={index} className="flex items-center">
+                            {Component}
+                        </div>
+                    ))}
+                </div>
+            )}
         </header>
     );
 };
 
 Header.propTypes = {
-    title: PropTypes.string,
+    title: PropTypes.string.isRequired,
     leftIcon: PropTypes.object,
-    onLeftClick: PropTypes.func,
+    onLeftClick: PropTypes.func.isRequired,
+    rightComponents: PropTypes.arrayOf(PropTypes.node),
 };
 
 export default Header;
--- src/pages/store/Menu.jsx
+++ src/pages/store/Menu.jsx
@@ -1,20 +1,51 @@
-import useSidebarStore from "../../stores/storePage/home/sidebarStore";
-import Header from "../../components/storePage/management/menu/Header";
+import useSidebarStore from "../../stores/sidebarStore";
+import Header from "../../components/storePage/home/Header";
 import Sidebar from "../../components/storePage/home/Sidebar";
 import CategorySection from "../../components/storePage/management/menu/CategorySection";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { useEffect } from "react";
+
 function Menu() {
     const toggleSidebar = useSidebarStore((state) => state.toggleSidebar);
+    const setTitle = useSidebarStore((state) => state.setTitle);
+    const title = useSidebarStore((state) => state.title);
     const merchantName = "海洋大學店";
-    //const isOpen = useSidebarStore((state) => state.isOpen);
-    //console.log(isOpen);
+    const onAddClick = () => { console.debug("add click") };
+
+    useEffect(() => {
+        setTitle("菜單管理");
+        console.debug("set menu title")
+    }, [setTitle]);
+
+    const addButton = (
+        <button
+            onClick={onAddClick}
+            className="bg-orange-500 text-white rounded-lg p-2 flex  shadow-md"
+        >
+            <FontAwesomeIcon icon={faPlus} size="md" />
+        </button>
+    );
+    const previewButton = (
+        <button
+            onClick={() => {
+                console.debug("preview click");
+            }}
+            className=" bg-slate-400 text-white rounded-lg px-3 py-1 font-sm shadow-md"
+        >
+            預覽
+        </button>
+    );
     return (
         <div>
             <Header
-                merchantName={merchantName}
-                onLeftClick={toggleSidebar} //sidebar
-                onAddClick={toggleSidebar}
-                onPreviewClick={toggleSidebar}
-            ></Header>
+                title={title}
+                onLeftClick={toggleSidebar}
+                rightComponents={[
+                    addButton,
+                    previewButton,
+                ]}
+            />
             <Sidebar
                 merchantName={merchantName}
             ></Sidebar>
@@ -22,7 +53,7 @@ function Menu() {
                 <CategorySection categoryName="主食" />
                 <CategorySection categoryName="素食" />
             </div>
-        </div>
+        </div >
     );
 }
 
